ab465625e8eb7d5d96d5c0a4b8b2ad860cf76b5c
Syed Talal Jilani <talaljilani79@gmail.com>
2023-07-01 19:29:22 +0500

    all responsiveness issues solve

--- src/Components/SkillBoard.jsx
+++ src/Components/SkillBoard.jsx
@@ -29,50 +29,50 @@ const SkillBoard = () => {
         <div className="col2">
           <div className="skillgrid">
             <div className="icon">
-              <BiLogoFlutter size={70} color="white" />
+              <BiLogoFlutter className="icons" size={70} color="white" />
               <p>Flutter</p>
             </div>
             <div className="icon">
-              <BiLogoReact size={70} color="white" />
+              <BiLogoReact size={70} color="white" className="icons"/>
               <p>React</p>
             </div>
             <div className="icon">
-              <BiLogoMongodb size={70} color="white" />
+              <BiLogoMongodb size={70} color="white" className="icons"/>
               <p>Mongodb</p>
             </div>
             <div className="icon">
-              <BiLogoNodejs size={70} color="white" />
+              <BiLogoNodejs size={70} color="white" className="icons"/>
               <p>NodeJs</p>
             </div>
             <div className="icon">
-              <BiLogoJavascript size={70} color="white" />
+              <BiLogoJavascript size={70} color="white" className="icons"/>
               <p>Javascript</p>
             </div>
             <div className="icon">
-              <BiLogoPhp size={70} color="white" />
+              <BiLogoPhp size={70} color="white" className="icons"/>
             </div>
             <div className="icon">
-              <SiKalilinux size={70} color="white" />
+              <SiKalilinux size={70} color="white" className="icons"/>
               <p>Kali Linux</p>
             </div>
             <div className="icon">
-              <BiLogoFirebase size={70} color="white" />
+              <BiLogoFirebase size={70} color="white" className="icons"/>
               <p>Firebase</p>
             </div>
             <div className="icon">
-              <SiExpress size={70} color="white" />
+              <SiExpress size={70} color="white" className="icons"/>
               <p>Express Js</p>
             </div>
             <div className="icon">
-              <SiCss3 size={70} color="white" />
+              <SiCss3 size={70} color="white" className="icons"/>
                 <p>CSS</p>
             </div>
             <div className="icon">
-              <SiTailwindcss size={80} color="white" />
+              <SiTailwindcss size={80} color="white" className="icons"/>
                 <p>TailwindCSS</p>
             </div>
             <div className="icon">
-              <SiMysql size={80} color="white" />
+              <SiMysql size={80} color="white"className="icons" />
                 <p>MySql</p>
             </div>
           </div>
