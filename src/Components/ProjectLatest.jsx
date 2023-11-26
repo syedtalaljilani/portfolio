18f3e39fa29a0052b00362cee3ab6b62d6176597
Syed Talal Jilani <72516408+syedtalaljilani@users.noreply.github.com>
2023-11-26 15:34:51 +0500

    Project 02 is added

--- src/Components/ProjectLatest.jsx
+++ src/Components/ProjectLatest.jsx
@@ -18,7 +18,7 @@ const ProjectLatest = () => {
      </div>
      <div className="project_grid">
      <ProjectCard title={'WaterUI'} desc={'TailwindCSS Reusable Components Library'} technology={'React TailwindCSS CodeMirror'} img={'/assets/project01.png'} livelink={'https://waterui.netlify.app/'} cachedlink={'https://github.com/syedtalaljilani/WaterUI'}/>
-     <ProjectCard title={'ProtectX'} desc={'Discord anti-crash bot'} technology={'HTML SCSS Python Flask'} img={'/assets/ProtectX.png'}/>
+     <ProjectCard title={'Fiyxer'} desc={'Digital Agency Website Template'} technology={'React TailwindCSS Swiper'} img={'/assets/project02.png'} livelink={'https://fiyxer.com'} cachedlink={'https://github.com/syedtalaljilani/Fiyxer'} />
      <ProjectCard title={'ChertNodes'} desc={'Minecraft servers hosting '} technology={'HTML SCSS Python Flask'} img={'/assets/ChertNodes.jpg'}/>
         </div>
     </>
